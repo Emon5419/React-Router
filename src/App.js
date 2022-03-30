078a349ba87ae8482bc157cbb5bc8e7d491607a9
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-03-30 11:22:36 +0600

    added postDetail Componenets

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from './components/NotFound/NotFound';
 import Header from './components/Header/Header';
 import FriendDetail from './components/FriendDetail/FriendDetail';
 import Posts from './components/Posts/Posts';
+import PostDetail from './components/PostDetail/PostDetail';
 
 function App() {
   return (
@@ -14,12 +15,26 @@ function App() {
       <Header></Header>
 
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/friends' element={<Friends></Friends>}></Route>
-        <Route path='/friend/:friendId' element={<FriendDetail></FriendDetail>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
-        <Route path="/posts" element={<Posts></Posts>}></Route>
+        <Route path='/' element={<Home></Home>}>
+
+        </Route>
+        <Route path='/friends' element={<Friends></Friends>}>
+
+        </Route>
+        <Route path='/friend/:friendId' element={<FriendDetail></FriendDetail>}>
+
+        </Route>
+        <Route path='/about' element={<About></About>}>
+
+        </Route>
+        <Route path='*' element={<NotFound></NotFound>}>
+
+        </Route>
+        <Route path="/posts" element={<Posts></Posts>}>
+          <Route path=':postId' element={<PostDetail></PostDetail>}>
+
+          </Route>
+        </Route>
       </Routes>
 
     </div>
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 const Posts = () => {
-   const [post, setPost] = useState([]);
+   const [posts, setPosts] = useState([]);
    useEffect(() => {
-      const url = `https://jsonplaceholder.typicode.com/posts`;
-      fetch(url)
+      fetch('https://jsonplaceholder.typicode.com/posts')
          .then(res => res.json())
-         .then(data => setPost(data));
+         .then(data => setPosts(data));
    }, []);
    return (
       <div>
-         <h2>Every Post FaceBook Ever Had: {post.length}</h2>
+         <h2>Every Post FaceBook Ever Had: {posts.length}</h2>
+         {
+            posts.map(post => <Link
+               key={post.id}
+               to={`/posts/${post.id}`}
+            >{post.id} </Link>)
+         }
+         <Outlet></Outlet>
       </div>
    );
 };
